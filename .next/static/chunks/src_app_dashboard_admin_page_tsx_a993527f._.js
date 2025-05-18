fe08ccb3c9f2282dd3e464950fd9548b3efed8e4
Jay Dhabs <89709155+ipeprah152@users.noreply.github.com>
2025-05-18 17:31:30 +0000

    Delete .next directory
    Signed-off-by: Jay Dhabs <89709155+ipeprah152@users.noreply.github.com>

--- .next/static/chunks/src_app_dashboard_admin_page_tsx_a993527f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_c859fdaf._.js",
-  "static/chunks/node_modules_fbc08e10._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_login_page_tsx_21f633bc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_a56a0c44._.js",
-  "static/chunks/src_17cd2184._.js"
-],
-    source: "dynamic"
-});
